ead6fff3c13c88069a795f2dd601d2a408574416
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-01-03 23:20:47 +0500

    passed isScroll prop to the navbar

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import  Navbar from "../components/Navbar";
+import BackgroundImage from '../assets/home.jpg';
+import MovieLogo from '../assets/homeTitle.jpg';
+import{FaPlay} from "react-icons/fa";
+import { AirOutlinedInfoCircle } from 'react-icons/ai';
+
+
+
 
 export default function () {
+  const [isScrolled, setIsScrolled]= useState(false)
+  window.onscroll = () =>{
+    setIsScrolled(window.pageYOffset===0?false:true);
+    return()=>(window.onscroll = null)
+  };
   return (
 <>
-<Navbar/>
+<Navbar isScrolled={isScrolled} />
+<div className='hero'>
+  
+
+</div>
 </>
   )
 }
