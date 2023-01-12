5527cc0e4d9d08ba0e3fec6e232888bd8abf963e
Rameezkhan47 <rameezmuhammadkhan47@gmail.com>
2023-01-12 22:59:12 +0500

    API for Adding Content to Favorites in MongoDB

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,1 +1,27 @@
-const User = require("../modelss")
+const NetflixUsers = require("../models/UserModel");
+const User = require("../models/UserModel");
+
+module.exports.addtoLikedMovies = async (req, res) => {
+  try {
+    const { email, data } = req.body;
+    const user = await NetflixUsers.findOne({ email });
+    if (user) {
+      const { favoriteContent } = user;
+      const contentAlreadyLiked = favoriteContent.find(
+        ({ id }) => (id = data.id)
+      );
+      if (!contentAlreadyLiked) {
+        await NetflixUsers.findByIdAndUpdate(
+          user._id,
+          {
+            favoriteContent: [...user.favoriteContent, data],
+          },
+          { new: true }
+        );
+      } else return res.json({ msg: "Content already added to favorites" });
+    } else NetflixUsers.create({ email, favoriteContent: [data] });
+    return res.json({ msg: "Content added to favorites", payload: data });
+  } catch (error) {
+    return res.json({ msg: "Error Adding Content" });
+  }
+};
